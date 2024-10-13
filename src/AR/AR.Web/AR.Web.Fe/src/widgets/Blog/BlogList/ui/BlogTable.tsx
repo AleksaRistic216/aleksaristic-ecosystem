dee769fdf1ca5c8dc469a09023244cfc04fc46ac
Aleksa Ristic <aristiccitsira@gmail.com>
2024-10-14 00:16:51 +0200

    Fixed blog error

--- src/AR/AR.Web/AR.Web.Fe/src/widgets/Blog/BlogList/ui/BlogTable.tsx
+++ src/AR/AR.Web/AR.Web.Fe/src/widgets/Blog/BlogList/ui/BlogTable.tsx
@@ -1,13 +1,30 @@
-import { firebaseApp } from "@/app/firebase"
-import { CircularProgress, Grid, LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
-import { get, getDatabase, orderByChild, orderByValue, query, ref } from "firebase/database"
-import { useEffect, useState } from "react"
-import { BlogTableContainerStyled } from "../styled/BlogTableContainerStyled"
-import { useRouter } from "next/router"
-import { Pin, PinDrop, PushPin } from "@mui/icons-material"
+import { firebaseApp } from '@/app/firebase'
+import {
+    CircularProgress,
+    Grid,
+    LinearProgress,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from '@mui/material'
+import {
+    get,
+    getDatabase,
+    orderByChild,
+    orderByValue,
+    query,
+    ref,
+} from 'firebase/database'
+import { useEffect, useState } from 'react'
+import { BlogTableContainerStyled } from '../styled/BlogTableContainerStyled'
+import { useRouter } from 'next/router'
+import { Pin, PinDrop, PushPin } from '@mui/icons-material'
 
 export const BlogTable = (): JSX.Element => {
-
     const router = useRouter()
     const [blogs, setBlogs] = useState<any[] | undefined>(undefined)
 
@@ -15,62 +32,75 @@ export const BlogTable = (): JSX.Element => {
         const db = getDatabase(firebaseApp)
         const q = query(ref(db, '/blogs'))
 
-        get(q).then((snapshot) => {
-            if (snapshot.exists()) {
-                setBlogs(snapshot.val())
-            } else {
-                console.log("No data available")
-            }
-        }).catch((error) => {
-            console.error(error)
-        })
-
+        get(q)
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    setBlogs(snapshot.val())
+                } else {
+                    console.log('No data available')
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+            })
     }, [])
-    
-    return (
-        blogs == undefined ?
-            <CircularProgress /> :
-            <Grid item sm={12}>
-                <BlogTableContainerStyled component={Paper}>
-                    <Table aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Title</TableCell>
-                                <TableCell align="right">Date (yyyy/MM/dd)</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {blogs
-                                .toSorted((x: any, y: any) => {
-                                    if (x.isPinned !== y.isPinned)
-                                        return y.isPinned - x.isPinned
-                                    
-                                    return x.date > y.date ? -1 : 1
-                                }).map((blog: any) => (
+
+    return blogs == undefined ? (
+        <CircularProgress />
+    ) : (
+        <Grid item sm={12}>
+            <BlogTableContainerStyled component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                            <TableCell align="right">
+                                Date (yyyy/MM/dd)
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {blogs
+                            .filter((x: any) => x != null)
+                            .toSorted((x: any, y: any) => {
+                                if (x.isPinned !== y.isPinned)
+                                    return y.isPinned - x.isPinned
+
+                                return x.date > y.date ? -1 : 1
+                            })
+                            .map((blog: any) => (
                                 <TableRow
                                     onClick={() => {
                                         router.push(`/blog/${blog.src}`)
                                     }}
                                     key={blog.title}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    sx={{
+                                        '&:last-child td, &:last-child th': {
+                                            border: 0,
+                                        },
+                                    }}
                                 >
                                     <TableCell component="th" scope="row">
-                                        {
-                                            blog.isPinned != null && blog.isPinned == true ?
-                                                <PushPin sx={{
+                                        {blog.isPinned != null &&
+                                        blog.isPinned == true ? (
+                                            <PushPin
+                                                sx={{
                                                     fontSize: `1em`,
                                                     transform: `translateY(3px)`,
                                                     marginRight: 1,
-                                                }} /> : null
-                                        }
+                                                }}
+                                            />
+                                        ) : null}
                                         {blog.title}
                                     </TableCell>
-                                    <TableCell align="right">{blog.date}</TableCell>
+                                    <TableCell align="right">
+                                        {blog.date}
+                                    </TableCell>
                                 </TableRow>
                             ))}
-                        </TableBody>
-                    </Table>
-                </BlogTableContainerStyled>
-            </Grid>
+                    </TableBody>
+                </Table>
+            </BlogTableContainerStyled>
+        </Grid>
     )
 }
